e82de82e503380b7559d1f1006253dc137c1b42a
Roussel Kamaha <rkamaha@elitetechfirst.com>
2020-09-29 12:28:12 -0400

    Init project

---
+++ src/main/java/org/codefirst/jenkins/JenkinsApplication.java
@@ -0,0 +1,22 @@
+package org.codefirst.jenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("hello")
+@SpringBootApplication
+public class JenkinsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsApplication.class, args);
+    }
+
+    @GetMapping
+    public String hello() {
+        return "hello";
+    }
+}
